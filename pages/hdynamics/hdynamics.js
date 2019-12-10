55a5e3162c1bd668cfa257f201d1133078f5cc48
unknown <unknown@example.com>
2019-12-10 17:13:31 +0800

    为

--- pages/hdynamics/hdynamics.js
+++ pages/hdynamics/hdynamics.js
@@ -5,62 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    list: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.hdynamics();
   },
+  hdynamics(){
+    wx.showToast({
+      title: '正在加载',
+      icon:'loading',
+      duration:10000
+    })
+    var that = this;
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    wx.request({
+      url: 'http://localhost:8080/getUser',
+      method: 'post',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success:function(res){
+        var list = res.data;
+        if (list == null) {
+          wx.showToast({
+            title: 'ErrorMessage',
+            icon: 'false',   //图标
+            duration: 1500  //提示的延迟的时间
+          })
+        } else {
+          that.setData({
+            list: list
+          })
+          console.log(res)
+          wx.hideToast()
+        }
+      }
+    })
   }
 })
